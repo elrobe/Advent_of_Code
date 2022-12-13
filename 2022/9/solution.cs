dd00a5dfb4d0717dd6ceccdc91ec776f01b932e2
elrobe <eliasroberg@gmail.com>
2022-12-13 13:45:13 -0500

    Solution for 2022 day 9 part 2

--- 2022/9/solution.cs
+++ 2022/9/solution.cs
@@ -8,6 +8,11 @@
 //
 // Part 1 -
 // How many unique spots does the tail visit when following the head?
+//
+// Part 2 -
+// The rope now has 9 tails, each following behind each other.
+// Given the new configuration of the rope, how many unique spots
+// does the 9th tail visit when following the rest of the rope?
 
 // To run:
 // I used .NET 7.0 x64 for macOS and Mono (https://www.mono-project.com/)
@@ -75,7 +80,12 @@ public class TileString {
     public void MoveHead(string direction) {
         // First, move the head
         this.Head.Move(direction);
+        this.MoveTail();
+    }
 
+    // Modularize for part 2 since each subsequent "tail" moves according to
+    // its head, though only the true head of the string moves
+    public void MoveTail() {
         // Now we compare the tail to the head...
         // If they're on the same spot, do nothing
         if (this.Head.Row == this.Tail.Row && this.Head.Col == this.Tail.Col) {
@@ -122,7 +132,8 @@ public class TileString {
 
 // Main method/class wrapper
 public class Solution {
-    public static void Main(string[] args) {
+    // Part 1 involves only a head and a tail
+    public static void Part1(string[] lines) {
         Tile head = new Tile(0, 0); // Both start at the origin
         Tile tail = new Tile(0, 0);
         Dictionary<string, bool> map = new Dictionary<string, bool>();
@@ -130,8 +141,6 @@ public class Solution {
 
         TileString headTail = new TileString(head, tail);
 
-        // Process as we parse
-        string[] lines = System.IO.File.ReadAllLines("input.txt");
         foreach (string line in lines) {
             string[] input = line.Split(" ");
             string direction = input[0];
@@ -147,9 +156,53 @@ public class Solution {
                 }
             }
         }
-
         // For part one, the total number of keys in the dictionary is the unique spaces
         // the tail visited during its journey
         System.Console.WriteLine("Part 1: " + map.Keys.Count);
+    }
+
+    // Part 2 involves a head and 9 tails, each tail following the one ahead of it
+    public static void Part2(string[] lines) {
+        // Same strategy as before: mark the unique spots, starting with the origin
+        Tile head = new Tile(0, 0);
+        Dictionary<string, bool> map = new Dictionary<string, bool>();
+        map.Add(head.ID, true);
+
+        // Except now we have 9 TileStrings [(H, 1), (1, 2), ..., (8, 9)]
+        TileString[] rope = new TileString[9];
+        for (int i = 0; i < rope.Length; i++) {
+            Tile tail = new Tile(0, 0);
+            rope[i] = new TileString(head, tail);
+            head = tail; // The head of the next is the tail of the previous
+        }
+
+        // Now onto processing: we move only the first TileString's head,
+        // then move the tail for all other pairs
+        foreach (string line in lines) {
+            string[] input = line.Split(" ");
+            string direction = input[0];
+            int spaces = int.Parse(input[1]);
+            
+            for (int i = 0; i < spaces; i++) {
+                TileString cur = rope[0];
+                cur.MoveHead(direction); // This also moves the tail
+                for (int j = 1; j < rope.Length; j++) {
+                    cur = rope[j];
+                    cur.MoveTail(); // Move only the tail, relative to the head, which should have moved
+                }
+                // "cur" should now be the last pair, thus the tail should be "9"
+                if (!map.ContainsKey(cur.Tail.ID)) {
+                    map.Add(cur.Tail.ID, true);
+                }
+            }
+        }
+
+        System.Console.WriteLine("Part 2: " + map.Keys.Count);
+    }
+
+    public static void Main(string[] args) {
+        string[] lines = System.IO.File.ReadAllLines("input.txt");
+        Part1(lines);
+        Part2(lines);
     }
 }
